31cb6b4dcae18b6f94efb893d8cc804907d7c86e
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-05-08 09:10:59 -0400

    Customizer, WIP

--- demo/builder.js
+++ demo/builder.js
@@ -1,10 +1,10 @@
-export const listIcons = (icons, html) => {
+export const listIcons = (icons, html, options = {width: 50, height: 50, hidden: 0}) => {
   let demos = [];
   for (let icon in icons) {
     if (icon === 'setCustomTemplateLiteralTag') continue;
     demos.push(html`
       <div>
-        <div>${icons[icon]({width: 50, height: 50, hidden: 0})}</div>
+        <div>${icons[icon](options)}</div>
         <span>${icon}</span>
       </div>
     `);
